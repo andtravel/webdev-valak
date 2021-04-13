40b88e1f8267ce427c465d0477b7914b4f5432a4
andrey <anovts@gmail.com>
2021-04-13 05:45:59 +0300

    design slider

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3875,12 +3875,26 @@
 
 $(function() {
 
-    $("#portfolio").slick({
-        arrows: true,
+    var portfolio = $("#portfolio");
+    
+    portfolio.slick({
+        arrows: false,
         dots: true,
         speed: 800,
         slidesToShow: 2,
         slidesToScroll: 2,
+    });
+    
+    $("#portfolio-arrow-prev").on("click", function(event) {
+        event.preventDefault();
+    
+        portfolio.slick('slickPrev');
+    });
+    
+    $("#portfolio-arrow-next").on("click", function(event) {
+        event.preventDefault();
+    
+        portfolio.slick('slickNext');
     });
 
 });
